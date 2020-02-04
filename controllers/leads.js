849139df13936fbd874d4655affa5e9ab1514042
faikar <ghassanfaikar@yahoo.com>
2020-02-04 15:15:44 +0700

    initial commit

--- controllers/leads.js
+++ controllers/leads.js
@@ -16,14 +16,17 @@ module.exports = {
         db.query(sql, data, (err, result)=>{
             if (err) {
                 res.json({success:false,message:'gagal post'})
+                console.log(err);
             } else {
                 res.json({success:true,message:'post'})
+                console.log(result);
+
             }
         })
     },
 
     get : (req,res) => {
-        let sql = 'select * from leads_form order by timestamp ASC'
+        let sql = 'select * from leads_form order by timestamp DESC'
 
         db.query(sql,(err,result)=>{
             res.send(result)
